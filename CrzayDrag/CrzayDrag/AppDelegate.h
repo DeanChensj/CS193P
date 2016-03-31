86c8e00b828b3367a64d667f3657c1afb9e4fd41
Dean Chen <463575680@qq.com>
2016-03-31 20:24:13 +0800

    first commit

---
+++ CrzayDrag/CrzayDrag/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CrzayDrag
+//
+//  Created by Dean Chen on 16/3/31.
+//  Copyright © 2016年 Dean Chen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CrzayDrag/CrzayDrag/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CrzayDrag
+//
+//  Created by Dean Chen on 16/3/31.
+//  Copyright © 2016年 Dean Chen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
